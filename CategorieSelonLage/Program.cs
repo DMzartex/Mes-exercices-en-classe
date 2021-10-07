bf5a898ad3d4c6dbdd9f88385c9bda71f5d2a53d
DMzartex <91669692+DMzartex@users.noreply.github.com>
2021-10-07 14:18:59 +0200

    Ajout du programme CategorieSelonLage

---
+++ CategorieSelonLage/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CategorieSelonLage
+{
+    class Program
+    {
+
+
+        static void CategorieSportive(int age, out string categorie)
+        {
+
+
+            
+
+            if(age > 0 && age < 130)
+            {
+                if(age == 6 || age == 7)
+                {
+                    categorie = " Vous êtes en Poussin !";
+                }
+                else if(age == 8 || age == 9 )
+                {
+                    categorie = " Vous êtes en Pupille !";
+                }else if(age == 10 || age == 11)
+                {
+                    categorie = "Vous êtes en Minime !";
+                }else if(age >= 12 && age <= 14)
+                {
+                    categorie = "Vous êtes en Cadet !";
+                }
+                else
+                {
+                    categorie = "Aucune categorie !";
+                }
+            }
+            else
+            {
+                categorie = "Vous n'avez pas l'age";
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+            int age=0;
+            string categorie;
+
+            
+            Console.WriteLine("Entrez votre age svp : ");
+            age = int.Parse(Console.ReadLine());
+            CategorieSportive(age, out categorie);
+            Console.WriteLine(categorie);
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
