9d7521fd65d561a18293f556b7c7fc609077053d
DMzartex <91669692+DMzartex@users.noreply.github.com>
2021-10-07 14:18:07 +0200

    Ajout du programme algoTable5

---
+++ AlgoTable5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AlgoTable5
+{
+    class Program
+    {
+        static void algoTable5 (out string resultat)
+        {
+            resultat = "0";
+            int valeur;
+            
+            for (int n = 1; n <= 100; n++)
+            {
+                valeur = n * 5;
+                resultat = resultat + "," + valeur;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            string resultat;
+            algoTable5(out resultat);
+            Console.WriteLine("La table de 5 jusqu'a 500 est :");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine(resultat);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
